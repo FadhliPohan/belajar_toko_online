799805540ec6e31779bda912b96c96a0aa2a49fc
FadhliPohan <muhammadfadly.mfd@gmail.com>
2022-10-09 17:42:36 +0700

    layouts fix

--- resources/views/layouts/menudashboard.blade.php
+++ resources/views/layouts/menudashboard.blade.php
@@ -3,7 +3,7 @@
         <!-- Add icons to the links using the .nav-icon class
           with font-awesome or any other icon font library -->
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="{{ URL::to('admin') }}" class="nav-link">
                 <i class="nav-icon fas fa-th"></i>
                 <p>
                     Dashboard
@@ -124,7 +124,7 @@
         <li class="nav-item">
             <a href="#" class="nav-link"
                 onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
+                                                     document.getElementById('logout-form').submit();">
                 <i class="nav-icon fas fa-sign-out-alt"></i>
                 <p>
                     Sign Out
